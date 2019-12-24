169e178a5b22c19e530ad0e829b2e3bdb19c2035
Your Name <you@example.com>
2019-12-24 22:26:27 +0800

    24号晚上

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -3,11 +3,59 @@
         <bread-crumb slot="header">
             <template slot="title">文章列表</template>
         </bread-crumb>
+        <!-- 放置一个表单容器 -->
+
+        <el-form style="padding-left:50px">
+            <el-form-item label="文章状态:">
+                <el-radio-group v-model="searchForm.status">
+                    <el-radio :label="5">全部</el-radio>
+                    <el-radio :label="0">草稿</el-radio>
+                    <el-radio :label="1">待审核</el-radio>
+                    <el-radio :label="2">审核通过</el-radio>
+                    <el-radio :label="3">审核失败</el-radio>
+
+                </el-radio-group>
+                <!-- {{searchForm}} -->
+            </el-form-item>
+            <el-form-item label="频道列表:">
+                <!-- {{channels}} -->
+                <el-select placeholder="请选择频道" v-model="searchForm.channel_id">
+                    <!-- label 是显示值 value是存储值 -->
+                    <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="时间选择:">
+                <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
+            </el-form-item>
+        </el-form>
     </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      searchForm: {
+        status: 5, // 默认选中全部
+        channel_id: null, // 默认不选中任何一个
+        dateRange: [] // 日期范围
+      },
+      channels: []// 接收频道数据
+    }
+  },
+  methods: {
+    // 获取所有的频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels() // 获取文章数据
+  }
 
 }
 </script>
